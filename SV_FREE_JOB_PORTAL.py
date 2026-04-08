88e1cc42320bab1b72a7ea73ffda84818b93d6bb
omkarnt2001 <omkarthotapal689@gmail.com>
2026-04-08 13:39:44 +0530

    SV_FREE_JOB_PORTAL.py

--- SV_FREE_JOB_PORTAL.py
+++ SV_FREE_JOB_PORTAL.py
@@ -299,4 +299,6 @@ def logout():
     session.clear()
     return redirect('/')
 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5011)), debug=False)
+if __name__=="__main__":
+    init_db()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5011)))
